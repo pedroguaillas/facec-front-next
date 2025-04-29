0e695b3255a983cf1fd058d30f93805a253ffebd
Pedro Guaillas <peter.tufi@gmail.com>
2025-04-29 11:16:11 -0500

    Add active menu color

--- components/ui/sidebar/Sidebar.tsx
+++ components/ui/sidebar/Sidebar.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
     menu: boolean;
@@ -6,6 +9,8 @@ interface Props {
 }
 
 const Sidebar = ({ menu, setMenu }: Props) => {
+
+    const pathname = usePathname()
 
     const handleCloseSidebar = () => {
         setMenu(false);
@@ -32,8 +37,12 @@ const Sidebar = ({ menu, setMenu }: Props) => {
                 <nav className="p-2">
                     <ul className={`${menu ? "transition-all duration-300 [&>li>a>h3]:block" : "[&>li>a>h3]:hidden group-hover:[&>li>a>h3]:block"} [&>li>a>h3]:text-white [&>li>a>svg]:stroke-white`}>
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex flex-row items-center gap-2 px-2 py-2" href='/dashboard'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/dashboard' ? 'bg-primary' : ''}`}
+                                href='/dashboard'
+                            >
                                 <svg fill="none" viewBox="0 0 22 22" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                                 </svg>
@@ -50,16 +59,24 @@ const Sidebar = ({ menu, setMenu }: Props) => {
                             </Link>
                         </li> */}
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex flex-row items-center gap-2 px-2 py-2" href='/products'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/products' ? 'bg-primary' : ''}`}
+                                href='/products'
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                 </svg>
                                 <h3>Productos</h3>
                             </Link></li>
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex gap-2 px-2 py-2" href='/orders'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/orders' ? 'bg-primary' : ''}`}
+                                href='/orders'
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                 </svg>
@@ -67,8 +84,12 @@ const Sidebar = ({ menu, setMenu }: Props) => {
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex gap-2 px-2 py-2" href='/shops'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/shops' ? 'bg-primary' : ''}`}
+                                href='/shops'
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                 </svg>
@@ -76,8 +97,12 @@ const Sidebar = ({ menu, setMenu }: Props) => {
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex gap-2 px-2 py-2" href='/customers'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/customers' ? 'bg-primary' : ''}`}
+                                href='/customers'
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                                 </svg>
@@ -94,8 +119,12 @@ const Sidebar = ({ menu, setMenu }: Props) => {
                             </Link>
                         </li> */}
                         <li>
-                            <Link onClick={handleCloseSidebar}
-                                className="rounded hover:bg-primaryhover flex gap-2 px-2 py-2" href='/reports'>
+                            <Link
+                                onClick={handleCloseSidebar}
+                                className={`rounded hover:bg-primaryhover flex gap-2 px-2 py-2 
+                                    ${pathname === '/reports' ? 'bg-primary' : ''}`}
+                                href='/reports'
+                            >
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
                                 </svg>
