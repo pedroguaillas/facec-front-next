5d042cb917fb09e8180c8e42535b4dd70fd1d451
Pedro Guaillas <peter.tufi@gmail.com>
2025-05-01 11:12:29 -0500

    Permit 0% retention

--- schemas/tax.schema.ts
+++ schemas/tax.schema.ts
@@ -5,5 +5,5 @@ export const taxSchema = z.object({
     code: z.coerce.number().min(1, "Seleccione el tipo de impuesto"),
     tax_code: z.string().min(1, { message: 'Seleccione una retención' }),
     base: z.string().min(1, { message: 'Escriba la base' }),
-    porcentage: z.number().min(1, { message: 'Escriba el %' }),
+    porcentage: z.number().min(0, { message: 'Escriba el %' }),
 });
