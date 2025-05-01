7b8dcdb9af4fef555f8c9ec510942bb6fc9d8549
Pedro Guaillas <peter.tufi@gmail.com>
2025-05-01 01:29:28 -0500

    Delete comment in button

--- app/(route)/customers/create/components/ButtonSubmit.tsx
+++ app/(route)/customers/create/components/ButtonSubmit.tsx
@@ -11,19 +11,6 @@ export const ButtonSubmit = () => {
             <Separate />
 
             <div className='flex justify-end'>
-                {/* <button
-                    type='submit'
-                    disabled={status.pending}
-                    className='bg-blue-500 hover:bg-blue-700 disabled:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center gap-2'
-                >
-                    {status.pending && (
-                        <FaSpinner className='animate-spin' />
-                    )}
-                    {!status.pending && (
-                        <FaSave />
-                    )}
-                    Guardar
-                </button> */}
                 <div className='w-28'>
                     <PrimaryButton label='Guardar' type='submit' action='store' isLoading={status.pending} />
                 </div>
