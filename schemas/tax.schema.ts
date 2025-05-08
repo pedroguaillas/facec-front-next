29397a063597a9509453abdbfe6046bf9e6f66dc
Pedro Guaillas <peter.tufi@gmail.com>
2025-05-08 00:42:43 -0500

    Update tax schema

--- schemas/tax.schema.ts
+++ schemas/tax.schema.ts
@@ -4,6 +4,14 @@ export const taxSchema = z.object({
     id: z.string(), // o number si usas timestamp
     code: z.coerce.number().min(1, "Seleccione el tipo de impuesto"),
     tax_code: z.string().min(1, { message: 'Seleccione una retención' }),
-    base: z.string().min(1, { message: 'Escriba la base' }),
-    porcentage: z.number().min(0, { message: 'Escriba el %' }),
+    // base: z.string().min(1, { message: 'Escriba la base' }),
+    base: z.string()
+        .min(1, { message: 'Escriba la base' })
+        .transform(Number)
+        .refine((val) => val >= 0, { message: 'La base no puede ser negativa' }),
+
+    porcentage: z.string()
+        .min(1, { message: 'Escriba el %' })
+        .transform(Number)
+        .refine((val) => val >= 0, { message: 'El % no puede ser negativo' }),
 });
